543c1facfcd5f0a992fcfb894fe099c48fb78882
DougPrates1987 <mz.dpsolutions@gmail.com>
2026-09-14 23:16:13 +0800

    Refresh offline cache for English version

--- gemas/sw.js
+++ gemas/sw.js
@@ -1,4 +1,4 @@
-const CACHE='gemas-v2';
+const CACHE='gemas-v3';
 const FILES=['./','./index.html','./manifest.webmanifest'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(

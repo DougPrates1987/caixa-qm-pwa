f2784440c1eae75137b1a3ed13c0a808e04cc799
DougPrates1987 <mz.dpsolutions@gmail.com>
2026-09-14 23:11:11 +0800

    Atualizar cache da calculadora de gemas

--- gemas/sw.js
+++ gemas/sw.js
@@ -1,5 +1,9 @@
-const CACHE='gemas-v1';
+const CACHE='gemas-v2';
 const FILES=['./','./index.html','./manifest.webmanifest'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)).then(()=>self.skipWaiting())));
-self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
-self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+self.addEventListener('activate',e=>e.waitUntil(
+  caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE && k.startsWith('gemas-')).map(k=>caches.delete(k)))).then(()=>self.clients.claim())
+));
+self.addEventListener('fetch',e=>e.respondWith(
+  fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html')))
+));
